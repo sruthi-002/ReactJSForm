7092713b838efadb50cbb8fcd565f47c1d5f0915
sruthi-002 <sruthiramesh270@gmail.com>
2022-09-06 17:29:26 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const root = ReactDOM.createRoot(
+    document.getElementById('root')
+  );
+  const element = <div className="header">
+    <h1>Student Mark Calculation</h1>
+    <label>Enter the Name :</label>
+    <input type="text" placeholder="Enter the name"></input><br></br><br></br>
+    <label>Enter the Tamil marks   :</label>
+    <input type="text" placeholder="Enter the Tamil marks"></input><br></br><br></br>
+    <label>Enter the English marks :</label>
+    <input type="text" placeholder="Enter the English marks"></input><br></br><br></br>
+    <label>Enter the Maths Marks   :</label>
+    <input type="text" placeholder="Enter the Maths Marks"></input><br></br><br></br>
+    <label>Enter the Science Marks :</label>
+    <input type="text" placeholder="Enter the Science Marks"></input><br></br><br></br>
+    <label>Enter the Social Marks  :</label>
+    <input type="text" placeholder="Enter the Social Marks"></input><br></br><br></br>
+    <input type="Submit"></input>
+  </div>
+  root.render(element);
